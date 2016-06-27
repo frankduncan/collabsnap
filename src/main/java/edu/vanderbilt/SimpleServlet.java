e75deac6d3e2f81c756fb73a14ef4fe86705e1ea
Frank Duncan <frank@kank.net>
2016-06-27 17:40:55 -0500

    Message sent to server via snap block

--- src/main/java/edu/vanderbilt/SimpleServlet.java
+++ src/main/java/edu/vanderbilt/SimpleServlet.java
@@ -1,13 +1,25 @@
+package edu.vanderbilt;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
  
-public class SimpleServlet extends HttpServlet {
+public class SimpleServlet {
+  static ConcurrentLinkedQueue<Message> queue = new ConcurrentLinkedQueue<Message>();
+
+  public static class Ping extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<h1>Test Response</h1>");
+      queue.offer(new Message());
+      System.out.println("Ping Successful, size of queue is: " + queue.size());
+      response.setContentType("text/html");
+      response.setStatus(HttpServletResponse.SC_OK);
     }
+  }
+
+  public static class Message {
+  }
 }
