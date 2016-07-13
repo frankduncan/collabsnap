a3ced1a0b7b3a684847b1dbbacdfd5ca1987fc6c
Frank Duncan <frank@kank.net>
2016-07-12 23:34:12 -0700

    Conditional receiving based on an attribute

--- src/main/java/edu/vanderbilt/SimpleServlet.java
+++ src/main/java/edu/vanderbilt/SimpleServlet.java
@@ -8,10 +8,24 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.*;
 
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+import org.w3c.dom.*;
+
 import java.util.concurrent.ConcurrentLinkedQueue;
  
 public class SimpleServlet {
   static ConcurrentLinkedQueue<Message> queue = new ConcurrentLinkedQueue<Message>();
+  static DocumentBuilderFactory dbf;
+  static DocumentBuilder db;
+  static {{
+    try {
+      dbf = DocumentBuilderFactory.newInstance();
+      db = dbf.newDocumentBuilder();
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  } }
 
   public static class Ping extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -23,9 +37,31 @@ public class SimpleServlet {
   }
 
   public static class Poll extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       System.out.println("Poll Successful, size of queue is: " + queue.size());
-      Message msg = queue.poll();
+      StringBuffer str = new StringBuffer();
+      String line = null;
+      try {
+        BufferedReader reader = request.getReader();
+        while((line = reader.readLine()) != null)
+          str.append(line);
+      } catch(Exception e) {
+        e.printStackTrace();
+      }
+      Message msg = null;
+      try {
+        Document doc = db.parse(new ByteArrayInputStream(str.toString().getBytes()));
+        if(doc.getDocumentElement().getFirstChild() != null) {
+          Rule rule = getRule(doc.getDocumentElement().getFirstChild());
+          msg = getMessageFollowingRule(rule);
+        } else {
+          msg = queue.poll();
+        }
+      } catch(Exception e) {
+        e.printStackTrace();
+        // Prototypes mean no error handling, yay!
+      }
+
       if(msg != null) {
         response.setContentType(msg.getContentType());
         response.setStatus(HttpServletResponse.SC_OK);
@@ -38,18 +74,74 @@ public class SimpleServlet {
     }
   }
 
+  public static Message getMessageFollowingRule(Rule rule) {
+    Message potential = null;
+    for(Message msg : queue) {
+      if(msg.ruleApplies(rule)) {
+        potential = msg;
+      }
+    }
+    if(potential != null) {
+      boolean successfullyRemoved = queue.remove(potential);
+      return successfullyRemoved ? potential : getMessageFollowingRule(rule);
+    } else {
+      return null;
+    }
+  }
+
+  private interface Rule {
+    public void load(Node node);
+    public boolean valid(Document sprite);
+  }
+
+  private static class VariableName implements Rule {
+    private XPathExpression xpath;
+
+    public void load(Node node) {
+      Element ele = (Element)node;
+      String name = ele.getAttribute("name");
+      String val = ele.getAttribute("value");
+      try {
+        this.xpath = XPathFactory.newInstance().newXPath().compile("/sprite/variables/variable[@name='" + name + "']/l/text() = '" + val + "'");
+      } catch(Exception e) {
+        e.printStackTrace();
+      }
+    }
+
+    public boolean valid(Document sprite) {
+      try {
+        return this.xpath.evaluate(sprite).equals("true");
+      } catch(Exception e) {
+        e.printStackTrace();
+      }
+      return false;
+    }
+  }
+
+  private static Rule getRule(Node node) {
+    Rule rule = null;
+    if(node.getNodeName().equals("variable-equal")) {
+      rule = new VariableName();
+    } else {
+      throw new RuntimeException("Don't know node type: " + node.getNodeName());
+    }
+    rule.load(node);
+    return rule;
+  }
+
   public static class PostSprite extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       StringBuffer str = new StringBuffer();
       String line = null;
       try {
         BufferedReader reader = request.getReader();
-        while ((line = reader.readLine()) != null)
+        while((line = reader.readLine()) != null)
           str.append(line);
-      } catch (Exception e) {
+      } catch(Exception e) {
         e.printStackTrace();
       }
       queue.offer(new SpriteMessage(str.toString()));
+      System.out.println(str.toString());
       System.out.println("Sprite Post Successful, size of queue is: " + queue.size());
       response.setContentType("text/html");
       response.setStatus(HttpServletResponse.SC_OK);
@@ -59,17 +151,28 @@ public class SimpleServlet {
   public interface Message {
     public abstract String getText();
     public String getContentType();
+    public boolean ruleApplies(Rule rule);
   }
 
   public static class PingMessage implements Message {
     public String getText() { return "ping"; }
     public String getContentType() { return "text/html"; }
+    public boolean ruleApplies(Rule rule) { return true; }
   }
 
   public static class SpriteMessage implements Message {
     String spriteXML;
-    SpriteMessage(String spriteXML) { this.spriteXML = spriteXML; }
+    Document spriteDocument;
+    SpriteMessage(String spriteXML) {
+      this.spriteXML = spriteXML;
+      try {
+        this.spriteDocument = db.parse(new ByteArrayInputStream(spriteXML.getBytes()));
+      } catch(Exception e) {
+        e.printStackTrace();
+      }
+    }
     public String getText() { return spriteXML; }
     public String getContentType() { return "application/xml"; }
+    public boolean ruleApplies(Rule rule) { return rule.valid(spriteDocument); }
   }
 }
