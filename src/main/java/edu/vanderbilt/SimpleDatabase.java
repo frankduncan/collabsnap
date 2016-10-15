cc9f551dd6e7c587fb983faaf9b35270a0eff715
Frank Duncan <frank@kank.net>
2016-10-15 06:29:21 -0500

    In-memory database of activities with simple object dump persistence

---
+++ src/main/java/edu/vanderbilt/SimpleDatabase.java
@@ -0,0 +1,60 @@
+package edu.vanderbilt;
+
+import edu.vanderbilt.data.Activity;
+
+import java.util.*;
+
+public class SimpleDatabase {
+  private static SimpleDatabase instance = null;
+  private static final String FILE_NAME = "collabsnapdump.out";
+
+  private List<Activity> activities;
+
+  private SimpleDatabase() {
+    if(new java.io.File(FILE_NAME).exists()) {
+      activities = new ArrayList<Activity>();
+    } else {
+      try {
+        java.io.ObjectInputStream ois = new java.io.ObjectInputStream(new java.io.FileInputStream(FILE_NAME));
+        activities = (List<Activity>)ois.readObject();
+        ois.close();
+      } catch(Exception e) {
+        e.printStackTrace();
+        // In the case of failure here, we just reset the server to brand new.
+        // That means if deserialization fails, we don't really care, and count on
+        // other means to be backing up the data.
+        activities = new ArrayList<Activity>();
+      }
+    }
+  }
+
+  private static void createInstance() {
+    instance = new SimpleDatabase();
+    new Thread(new Save()).start();
+  }
+
+  public static SimpleDatabase instance() {
+    if(instance == null) {
+      synchronized(SimpleDatabase.class) {
+        if(instance == null) {
+          createInstance();
+        }
+      }
+    }
+    return instance;
+  }
+
+  private static class Save implements Runnable {
+    public void run() {
+      try {
+        Thread.sleep(5 * 60 * 1000);
+        java.io.ObjectOutputStream oos = new java.io.ObjectOutputStream(new java.io.FileOutputStream(FILE_NAME));
+        oos.writeObject(instance.activities);
+        oos.close();
+      } catch(Exception e) {
+        System.err.println("While saving, something very bad happened");
+        e.printStackTrace();
+      }
+    }
+  }
+}
---
+++ src/main/java/edu/vanderbilt/data/Activity.java
@@ -0,0 +1,5 @@
+package edu.vanderbilt.data;
+
+public class Activity implements java.io.Serializable {
+  public String name;
+}
